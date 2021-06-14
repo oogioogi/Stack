935b12125976d81b7360ae24294bf0b9400ba24f
oogioogi <detacl73@gmail.com>
2021-06-14 20:07:41 +0900

    -Text

--- Stack/ContentView.swift
+++ Stack/ContentView.swift
@@ -10,79 +10,27 @@ import SwiftUI
 
 // Stack
 // H V Z
+
+// Text
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack{
+            Text("Hello World")
+                .font(.largeTitle)
+                .underline(true, color: .blue)
+                .bold()
+                .background(Color.yellow)
+            Text("Mr Lee")
+                .frame(width: 150, height: 50, alignment: .center)
+                .background(Color.green)
+                .cornerRadius(15.0)
+            
+        }
+
     }
 }
-/* [ Stack ]
-struct ContentView: View {
-    var body: some View {
-        // ------ Stack ------
-        //        VStack{
-        //            Text("Hello, world!")
-        //            Text("Hello, world!")
-        //            Text("Hello, world!")
-        //        }
-        //        HStack{
-        //            Text("Hello, world!")
-        //            Text("Hello, world!")
-        //            Text("Hello, world!")
-        //        }
-        // ---------------------
-        // ----- Text -------
-        ZStack {
-            Color
-                .blue
-                .edgesIgnoringSafeArea(.all)
-            VStack{
-                HStack{
-                    Text("7")
-                        .font(.largeTitle)
-                        .foregroundColor(.red)
-                        .padding(.trailing, 2)
-                    Text("8")
-                        .font(.largeTitle)
-                        .foregroundColor(.red)
-                        .padding(.trailing, 2)
-                    Text("9")
-                        .font(.largeTitle)
-                        .foregroundColor(.red)
-                        .padding(.trailing, 2)
-                }.padding(.bottom,1)
-                HStack{
-                    Text("4")
-                        .font(.headline)
-                        .foregroundColor(.red)
-                        .padding(.trailing, 2)
-                    Text("5")
-                        .font(.callout)
-                        .foregroundColor(.red)
-                        .padding(.trailing, 2)
-                    Text("6")
-                        .font(.subheadline)
-                        .foregroundColor(.red)
-                        .padding(.trailing, 2)
-                }.padding(.bottom,1)
-                HStack{
-                    Text("1").padding(.trailing, 2)
-                    Text("2").padding(.trailing, 2)
-                    Text("3").padding(.trailing, 2)
-                }.padding(.bottom,1)
-                HStack{
-                    Text("0").padding(.trailing, 2)
-                    Text(".").padding(.trailing, 2)
-                    Text(" ").padding(.trailing, 2)
-                }
-            }.padding(.bottom, 250)
-            
-            VStack{
-                Text("Hello World")
-            }
-        }
-    }
-    
-}*/
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
