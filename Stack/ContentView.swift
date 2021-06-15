978c1926911d784c0bcd7d8bf88964bdc449d019
oogioogi <detacl73@gmail.com>
2021-06-15 19:12:54 +0900

    -Button

--- Stack/ContentView.swift
+++ Stack/ContentView.swift
@@ -11,47 +11,42 @@ import SwiftUI
 // Stack
 // H V Z
 
-// Text
-
 struct ContentView: View {
+    @State private var isOn = false
+    
+    var widths: CGFloat {
+        if isOn {
+            return 80
+        }else {
+            return 100
+        }
+    }
+    
+    var heights: CGFloat {
+        if isOn {
+            return 80
+        }else {
+            return 100
+        }
+    }
+    
     var body: some View {
         VStack{
-            Image("face-mask")
-                .resizable()
-                .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .background(Color.gray)
-                .mask(Circle())
-            
             Button(action: {
-                print("Tapped 로그인")
-            }) {
-                Text("로그인")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .bold()
-                    .frame(width: 150, height: 50, alignment: .center)
-                    .background(Color.yellow)
-                    .cornerRadius(15.0)
-                    .padding(10)
-            }
-            
-            Button(action: {
-                print("Tapped 회원 가입")
-            }) {
-                Text("회원 가입")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .bold()
-                    .frame(width: 150, height: 50, alignment: .center)
-                    .background(Color.yellow)
-                    .cornerRadius(15.0)
-                    .padding(.bottom, 10)
-            }
-            Text("회원 가입은 여기서....")
-                .font(.caption)
-                .underline()
-                .foregroundColor(.pink)
-        }
+                isOn.toggle()
+            }, label: {
+                VStack{
+                    Image("running-man")
+                        .resizable()
+                        .frame(width: widths, height: heights, alignment: .center)
+                        .background(Color.gray)
+                        .cornerRadius(20.0)
+                    Text(isOn.description)
+                }
 
-        
+                
+            })
+        }
     }
 }
 
